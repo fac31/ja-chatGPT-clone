bee3bf2241d7c729434a5c11b0075b23b1973721
Angelica <Angelica137@users.noreply.github.com>
2024-05-07 21:46:10 +0100

    check all routes are ok

--- app.js
+++ app.js
@@ -1,13 +1,13 @@
 const express = require("express");
-
 const app = express();
-
 const path = require("path");
 
-app.use(express.static(path.join(__dirname)));
+// Serve static files from the 'public' directory
+app.use(express.static(path.join(__dirname, "public")));
 
+// Route to serve the HTML file when accessing the root URL
 app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "ChatGPT.html"));
+  res.sendFile(path.join(__dirname, "public", "ChatGPT.html"));
 });
 
 app.listen(3000, () => {
