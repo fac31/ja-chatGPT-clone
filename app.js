8e6cad3c13237eccf89b08ac15dc42e458fcfc76
Angelica <Angelica137@users.noreply.github.com>
2024-05-09 19:57:27 +0100

    require node-fetch

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require("express");
-const app = express();
+const fetch = require("node-fetch");
 const path = require("path");
+
+const app = express();
 
 // Serve static files from the 'public' directory
 app.use(express.static(path.join(__dirname, "public")));
