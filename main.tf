eb93d84f703d6df8f8aba6f6b4423ea6fa79e633
Angelica <Angelica137@users.noreply.github.com>
2024-05-18 14:10:25 +0100

    try to fix the ssh

--- main.tf
+++ main.tf
@@ -2,12 +2,22 @@ provider "aws" {
   region = "us-east-1"
 }
 
-
 resource "aws_instance" "ja-chatGPT-clone" {
-  ami           = "ami-0bb84b8ffd87024d8"
+  ami           = "ami-0001b97ee748d91f6"
   instance_type = "t2.micro"
+	key_name = "ja-chatGPT-clone"
 
   tags = {
     Name = "JAChatGPTClone"
   }
+}
+
+
+// try to see route with output
+output "instance_id" {
+  value = aws_instance.ja-chatGPT-clone.id
+}
+
+output "instance_public_ip" {
+  value = aws_instance.ja-chatGPT-clone.public_ip
 }
